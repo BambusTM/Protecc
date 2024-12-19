62b13bbf6d727aa3acac59f76f8d827ce73ec03a
BambusTM <81088539+BambusTM@users.noreply.github.com>
2024-12-19 08:51:23 +0100

    added fontawsome icons (wont display smh)

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -14,6 +14,10 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                
+                fonts.AddFont("FontAwesomeBrands.otf", "FontAwesomeBrands");
+                fonts.AddFont("FontAwesomeRegular.otf", "FontAwesomeRegular");
+                fonts.AddFont("FontAwesomeSolid.otf", "FontAwesomeSolid");
             })
             .UseMauiCommunityToolkit();
 
