54920549fb9c4928b7b5b7976d7836e50777f410
BambusTM <81088539+BambusTM@users.noreply.github.com>
2025-01-02 17:38:04 +0100

    fixed layout and icons, top now

---
+++ StatisticsPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protecc;
+
+public partial class StatisticsPage : ContentView
+{
+    public StatisticsPage()
+    {
+        InitializeComponent();
+    }
+}
